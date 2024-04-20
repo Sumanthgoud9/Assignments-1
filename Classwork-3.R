6c53da3113816a9c15b2f481c8cd47646262fee7
Sumanthgoud9 <166311900+Sumanthgoud9@users.noreply.github.com>
2024-04-19 20:14:24 -0400

    4-12-24 Basic Stats

---
+++ Classwork-3.R
@@ -0,0 +1,38 @@
+#1.Average of Three Numbers
+
+#Initialise the variables
+a<-7
+b<-12
+c<-5
+#calculate the Average
+average=(a+b+c)/3
+#print the average
+print(paste("average of",a,b,c,":",average))
+
+
+#2.printout "Welcome to R, <first Name>"
+
+#Initialise the varaiable
+first_name<-"Sumanth Goud"
+#printing
+print(paste("Welcome to R, ", first_name))
+      
+#3.Find out which is bigger
+
+#Initialise the varaiables
+x<-7
+y<-12
+z<-4
+# Checking if x is bigger than y or not
+if (x > y) {
+  print("x is bigger than y")
+} else {
+  print("x is not bigger than y")
+}
+# Checking if x is bigger than z or not
+if (x > z) {
+  print("x is bigger than z")
+} else {
+  print("x is not bigger than z")
+}
+
