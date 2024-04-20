9d911caeea99b462b3249ede0f0d46d711020531
Sumanthgoud9 <166311900+Sumanthgoud9@users.noreply.github.com>
2024-04-19 20:22:09 -0400

    4-19-24_Basic Stats

---
+++ Classwork-3.R
@@ -0,0 +1,24 @@
+
+N <-100000
+a <-rnorm(N)
+b <- rnorm(N)
+
+#Vectorized approach
+c <- a * b
+c
+
+
+
+X <- rnorm(5)
+X <- rnorm( 5, 10, 1)
+X <- rnorm (5, 10)
+X <- rnorm(5, mean=10, sd = 1)
+X <- rnorm(5, sd=1, mean=10)
+X <- rnorm(5, sd=1)
+
+x <-rnorm(10,2,1)
+plot(x)
+hist(x)
+boxplot(x, horizontal= TRUE)
+
+
