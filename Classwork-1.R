e5243a1c4782745d472303aa1726e66c6db1969d
Sumanthgoud9 <166311900+Sumanthgoud9@users.noreply.github.com>
2024-05-03 16:08:53 -0400

    4-5-24_Classwork-1

---
+++ Classwork-1.R
@@ -0,0 +1,71 @@
+"Hello, World"
+
+A<-5
+B<-10
+C<-A+B
+C
+
+greet<-"Hello,"
+name<-" Bob"
+message<-paste(greet,name)
+message
+
+#Whole Number
+X<-2L
+typeof(X)
+
+#two equal
+y<-2.5
+typeof(y)
+
+#composite
+Z<-2+3i
+typeof(Z)
+
+#character
+a<-"h"
+typeof(a)
+
+#Reasoning
+#q<-T
+typeof(q)
+p<-FALSE
+typeof(p)
+
+BasicMath
+a<-5
+b<-3
+c<-a+b
+d<-a-b
+e<-a*b
+f<-a/b
+
+a
+b
+c
+d
+e
+f
+
+
+#BAsic Logic
+4<5
+10>100
+4==5
+
+result<-4<5
+result
+typeof(result)
+
+result2<-!(5>1)
+result2
+
+result|result2
+result & result2
+
+isTRUE(result)
+_
+
+
+
+
